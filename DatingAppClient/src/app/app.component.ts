cf9c827386994e4ad0c6455bc523f0302922a321
darioMiskovic <dario12898@gmail.com>
2021-11-06 20:30:02 +0100

    frontend

---
+++ DatingAppClient/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'DatingAppClient';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:44380/api/users').subscribe(
+      (res) => {
+        this.users = res;
+        console.log(this.users);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
